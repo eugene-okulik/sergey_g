77ccf4ff3f303b6277a73dd1124d94bdef9cc4f6
serg.g <kurchuk@mail.ru>
2023-11-15 11:54:22 +0300

    Update task_1.py

--- homework/sergey_g/homework_6/task_1.py
+++ homework/sergey_g/homework_6/task_1.py
@@ -3,24 +3,23 @@
 # imperdiet vel. Integer urna nisl, facilisis vitae semper at, dignissim vitae libero”
 # и после этого выводит получившийся текст на экран.
 
-text = 'Etiam tincidunt neque erat, quis molestie enim imperdiet vel. Integer urna nisl, ' \
-       'facilisis vitae semper at, dignissim vitae libero'
-print(text)
+text = (
+        'Etiam tincidunt neque erat, quis molestie enim imperdiet vel. Integer urna nisl, '
+        + 'facilisis vitae semper at, dignissim vitae libero'
+)
+print(f'Исходная версия текста: {text}')
 
-symbol_del_1 = ','
-symbol_del_2 = '.'
-not_symbol_1 = text.replace(symbol_del_1, '')
-new_text = not_symbol_1.replace(symbol_del_2, '')
+words = text.split()
+result = []
 
-words = new_text.split()
+for word in words:
+    if word.endswith(','):
+        word = word[:-1] + 'ing,'
+    elif word.endswith('.'):
+        word = word[:-1] + 'ing.'
+    else:
+        word = word + 'ing'
+    result.append(word)
 
-for i in range(len(words)):
-    words[i] += 'ing'
-
-new_text = ' '.join(words)
-
-text = new_text.replace("nisling", "nisling,")
-text = text.replace("ating", "ating,")
-fin_text = text.replace("veling", "veling.")
-
-print(fin_text)
+result_text = ' '.join(result)
+print(f'Редактированный текст : {result_text}')
