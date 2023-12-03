6a7754aa47852fdd790d177c04d46ce13a0cfa49
serg.g <kurchuk@mail.ru>
2023-12-03 11:48:21 +0300

    homework_8

---
+++ homework/sergey_g/homework_8/task_1.py
@@ -0,0 +1,19 @@
+# Задание 1
+# Напишите программу. Есть две переменные, salary и bonus. Salary - int, bonus - bool. Спросите у пользователя salary. А bonus пусть назначается рандомом.
+#
+# Если bonus - true, то к salary должен быть добавлен рандомный бонус.
+#
+# Примеры результатов:
+#
+# 10000, True - '$10255'
+# 25000, False - '$25000'
+# 600, True - '$3785'
+
+import random
+
+salary = int(input("Введите число: "))
+bonus = random.choice([True, False])
+value = f'{salary}, {bonus}'
+if bonus:
+    salary += random.randint(100, 1000)
+print(f" {value} - '${salary}'")
